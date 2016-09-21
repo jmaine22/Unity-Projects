be561f98e84315ba19e83ef347f78674bdc69870
jmaine22 <users@user-ss-MacBook-818.local>
2016-09-20 22:40:15 -0700

    Added initial project file
    Added the initial project files from unity to the repository. I also
    added my task magnet debris to the repository.

---
+++ Assets/Code/magnetDebris.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class magnetDebris : MonoBehaviour 
+{
+	/*The*/
+	public GameObject player;
+	public bool magnetTrigger = false;
+	public float mSpeed = 30f;
+	public Vector3 transferDirection;
+	public ParticleSystem particle;
+
+	// Use this for initialization
+	void Start () 
+	{
+		//Get a reference of the player
+		player = GameObject.Find("player");
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		Debug.Log ("Object entered the trigger");
+		//Activate Trigger Zone
+		magnetTrigger = true;
+
+	}
+	void OnTriggerStay(Collider other)
+	{
+		Debug.Log ("Object stayed at the trigger");
+		//When trigger is activated move the particles
+		//
+		ParticleSystem.Particle []ParticleList = new ParticleSystem.Particle[particle.particleCount];
+		particle.GetParticles (ParticleList);
+		for (int i = 0; i < ParticleList.Length; ++i) 
+		{
+			ParticleList [i].velocity = new Vector3 (0, 5f, 0);
+		}
+
+		particle.SetParticles (ParticleList, particle.particleCount);
+		//Determine how much gravity to apply to particle system.
+		particle.gravityModifier = 10;
+	}
+	void OnTriggerExit(Collider other)
+	{
+		//Test to see if the trigger work
+		Debug.Log ("Object exit the trigger");
+		//Determine how much gravity to apply to particle system.
+		particle.gravityModifier = 0;
+	}
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+}
---
+++ Assets/Code/playerController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playerController : MonoBehaviour 
+{
+	public float h;
+	public float v;
+	public float wSpeed = 50f;
+	public float rSpeed = 50f;
+	public float movementDistance;
+	public float z;
+	public Animator player;
+
+	// Use this for initialization
+	void Start () 
+	{
+		player = GetComponent<Animator>();
+		z = transform.position.z;
+		movementDistance = 10;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		v = Input.GetAxis("Vertical");
+		h = Input.GetAxis("Horizontal");
+	}
+
+	void FixedUpdate () 
+	{
+		transform.Translate(new Vector3 (h * Time.deltaTime * wSpeed, 0, v * Time.deltaTime * wSpeed));
+		//transform.Translate(new Vector3 (h, 0 * Time.deltaTime * rSpeed, 0));
+	}
+}
